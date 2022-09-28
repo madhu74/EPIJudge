a11a3dc494a00c0074755370bfd9f6eb7b65bee6
madhu74 <madhusudan600019@gmail.com>
2022-09-27 22:52:54 -0500

    parity bit solution

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,10 +1,24 @@
 from test_framework import generic_test
 
 
-def parity(x: int) -> int:
+def parity(x: int) -> int: # my solution
     # TODO - you fill in here.
-    return 0
+    counter = 0
+    while x:
+        counter = counter + (x&1)
+        x = x >> 1
+    return counter % 2
 
+def parity_(x:int) ->int:# book's brute solution
+    result = 0
+    while x:
+        result = result ^ (x & 1)
+        # print("Each level--->", result)
+        x = x>> 1
+    # print(result)
+    return result
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
+    # r = parity(15)
+    # print(r)
