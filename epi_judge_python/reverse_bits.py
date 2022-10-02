643968a0d1447df4415d067a3c809da02a785339
madhu74 <madhusudan600019@gmail.com>
2022-10-02 00:39:45 -0500

    wrong solution, neet to revisit this reverse bit

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -2,11 +2,22 @@ from test_framework import generic_test
 
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # Note - This solution is wrong and need to understand lookups a bit more
+    # approach
+    x = bin(x) # binary conversion
+    x = str(x)[2:]
+    x = x[::-1] # reverse
+    idx = 0
+    while idx < len(x): # remove leading 0's since it is binary
+        if x[idx] == '1':
+            break
+        idx = idx+1
+    # print(x[idx:].ljust(64, '0'))
+    return int(x[idx:].ljust(64, '0'), base=2) # do int conversion ljust 64 since it is a 64 it is number
 
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('reverse_bits.py', 'reverse_bits.tsv',
                                        reverse_bits))
+    # 1351510410656   
